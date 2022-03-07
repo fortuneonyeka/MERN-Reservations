7eaaff2d821f1dc5c147cc5d91af83eae06e3975
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-03-07 17:35:37 +0100

    create user model

---
+++ server/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    age: {
+      type: Number,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    username: {
+      type: String,
+      required: true
+    }
+})
