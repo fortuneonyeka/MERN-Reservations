d4a90134ce9902e8c411605b4ed2f23f9a2f2a2d
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-03-10 11:22:31 +0100

    create registeration endpoint

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,3 +11,42 @@ const validateLoginInput = require("../../validation/login")
 
 //Load User model
 const User = require("../../server/models/User")
+
+
+// @route POST api/users/register
+// @desciption Register user
+// @access Public
+
+router.post("/register", (req, res) => {
+   // Form validation
+
+   const { errors, isValid } = validateRegisterInput(req.body);
+
+   //Check validations
+   if (!isValid) {
+     res.status(400).json(errors)
+   }
+
+   User.findOne({email:req.body.email}).then(user => {
+     if (user) {
+       res.status(400).json({email:"Email exists already"})
+     }else {
+       const newUser = new User({
+         name: req.body.name,
+         email: req.body.email,
+         password: req.body.email,
+         username: req.body.username,
+         age: req.body.age
+       })
+
+       // Hash password before saving in database
+       bcrypt.genSalt(10, (err, salt) => {
+         bcrypt.hash(newUser.password, salt, (err, hash) => {
+           if(err) throw err;
+            newUser.password = hash;
+            newUser.save.then(user => res.json(user)).catch(err => console.log(err));
+         });
+       });
+     }
+   });
+});
